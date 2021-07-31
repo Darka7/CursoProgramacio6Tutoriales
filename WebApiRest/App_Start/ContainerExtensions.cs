7c1583e7a75083cd0778d1539d2cb9f4ccafcdd6
Andrey Salazar Guzman <asalazar@grupomas.com>
2021-07-30 18:31:06 -0600

    mejoras

---
+++ WebApiRest/App_Start/ContainerExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BD;
+using WBL;
+namespace WebApiRest
+{
+    public static class ContainerExtensions
+    {
+
+        public static IServiceCollection AddDIContainer(this IServiceCollection services)
+        {
+
+            services.AddSingleton<IDataAccess,DataAccess>();
+            services.AddTransient<IUsuariosServices, UsuariosServices>();
+            services.AddTransient<IClientesService, ClientesService>();
+
+
+            return services;
+        }
+    }
+}
